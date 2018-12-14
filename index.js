9af5e50b8704f2a9d40a109740e19becc04217a6
Yura Kushnir <kushnir.yura@gmail.com>
2018-12-14 14:54:12 +0200

    Add initial population

--- index.js
+++ index.js
@@ -1,31 +1,65 @@
 /* Refer to https://github.com/OleksiyRudenko/a-tiny-JS-world for the task details
    Complete the below for code reviewers' convenience:
 
-   Code repository: _put repo URL here_
-   Web app: _put project's github pages URL here_
+   Code repository: https://github.com/rrrds/a-tiny-JS-world
+   Web app: https://rrrds.github.io/a-tiny-JS-world/
    */
 
+function toString(obj) {
+    const values = [];
+
+    for (let prop in obj) {
+        if (obj.hasOwnProperty(prop)) {
+            values.push(obj[prop]);
+        }
+    }
+
+    return values.join('; ');
+}
+
 // ======== OBJECTS DEFINITIONS ========
-// Define your objects here
+const dog = {
+    species: 'dog',
+    legs: 4,
+    hands: 0,
+    name: 'Lola',
+    gender: 'female',
+    say: 'Woof!'
+}
+
+const cat = {
+    species: 'cat',
+    legs: 4,
+    hands: 0,
+    name: 'Toger',
+    gender: 'male',
+    say: 'Meow!'
+}
+
+const woman = {
+    species: 'human',
+    legs: 2,
+    hands: 2,
+    name: 'Ann',
+    gender: 'female',
+    say: 'Hello! I\'m Ann!'
+}
+
+const man = {
+    species: 'human',
+    legs: 2,
+    hands: 2,
+    name: 'John',
+    gender: 'male',
+    say: 'Hello! I\'m John!'
+}
+
+const catWoman = Object.assign({}, cat, {species: 'cat woman', legs:2, hands:2, name: 'Selina Kyle', gender: 'female'});
 
 
 // ======== OUTPUT ========
-/* Use print(message) for output.
-   Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
-
-   Message can contain HTML markup. You may also tweak index.html and/or styles.css.
-   However, please, REFRAIN from improving visuals at least until your code is reviewed
-   so code reviewers might focus on a single file that is index.js.
-   */
-
-/* Print examples:
-   print('ABC');
-   print('<strong>ABC</strong>');
-   print('<strong>ABC</strong>', 'div');
-
-   print('human; John; male; 2; 2; Hello world!; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
-   */
-
-
+print(toString(dog));
+print(toString(cat));
+print(toString(man));
+print(toString(woman));
+print(toString(catWoman));
